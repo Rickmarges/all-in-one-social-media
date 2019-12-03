f91cfdc88c7b9038eb31e83e69003a035fb4b93b
MarynKaspers <marynkaspers20@gmail.com>
2019-12-03 14:35:23 +0100

    Added Animations
    Animated errors login

--- Dash/app/src/main/java/com/example/dash/ui/login/LoginActivity.java
+++ Dash/app/src/main/java/com/example/dash/ui/login/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -13,6 +15,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dash.ui.dashboard.DashboardActivity;
 import com.example.dash.ui.register.RegisterActivity;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.example.dash.R;
 
@@ -55,30 +59,21 @@ public class LoginActivity extends AppCompatActivity {
 
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            Animation animShake = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.hshake);
+            emailTV.startAnimation(animShake);
             progressBar.setVisibility(View.GONE);
             return;
         }
         if (TextUtils.isEmpty(password)) {
             Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            Animation animShake = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.vshake);
+            passwordTV.startAnimation(animShake);
             progressBar.setVisibility(View.GONE);
             return;
         }
 
         mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        Log.d("Login", "Successful");
-                        progressBar.setVisibility(View.GONE);
-
-                        Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
-                        startActivity(intent);
-                    }
-                    else {
-                        Log.d("Login", "Failed");
-                        Toast.makeText(getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
-                        progressBar.setVisibility(View.GONE);
-                    }
-                });
+                .addOnCompleteListener(this::onComplete);
     }
 
     private void initializeUI() {
@@ -87,5 +82,21 @@ public class LoginActivity extends AppCompatActivity {
         registerBtn = findViewById(R.id.register);
         loginBtn = findViewById(R.id.login);
         progressBar = findViewById(R.id.loading);
+    }
+
+    private void onComplete(Task<AuthResult> task) {
+        if (task.isSuccessful()) {
+            Log.d("Login", "Successful");
+            progressBar.setVisibility(View.GONE);
+
+            Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+            startActivity(intent);
+        } else {
+            Log.d("Login", "Failed");
+            Toast.makeText(getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
+            Animation animShake = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.hshake);
+            loginBtn.startAnimation(animShake);
+            progressBar.setVisibility(View.GONE);
+        }
     }
 }
