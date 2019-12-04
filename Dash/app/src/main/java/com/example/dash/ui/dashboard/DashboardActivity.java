670a5f0278b12e25998830a34ada1e8d034e3ab0
Rickmarges <rickmarges@gmail.com>
2019-12-04 19:41:41 +0100

    Major changes
    Added email verification. It's also required to login. Once you make an account it automatically sends a verificationmail. Once your email is verificated you can login.  (This does mean you NEED an ACTIVE email account to create an account or test the registration/login)
    
    Changed back button functionality.  You need to press the back button twice to close the app on the dashboard and login screen. If the back button is pressed on the register page, you'll be brought back to the login.
    
    Added red required items to fields when left blank.
    
    Changed cursor color on the input fields to make them vissible

--- Dash/app/src/main/java/com/example/dash/ui/dashboard/DashboardActivity.java
+++ Dash/app/src/main/java/com/example/dash/ui/dashboard/DashboardActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.example.dash.R;
 import com.example.dash.ui.login.LoginActivity;
@@ -16,6 +17,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class DashboardActivity extends AppCompatActivity {
     private Button signOutBtn;
     private FirebaseUser user;
+    private int backCounter;
+    private long startTime;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,21 +32,35 @@ public class DashboardActivity extends AppCompatActivity {
         initialize();
     }
 
+    @Override
+    public void onBackPressed(){
+        if (backCounter < 1 || (System.currentTimeMillis() - startTime) / 1000 > 3) {
+            startTime = System.currentTimeMillis();
+            Toast.makeText(getApplicationContext(), "Press again to exit", Toast.LENGTH_SHORT).show();
+            backCounter++;
+        } else {
+            backCounter = 0;
+            finishAffinity();
+        }
+    }
+
     private void initialize(){
         setContentView(R.layout.activity_dashboard);
+
+        backCounter = 0;
 
         initializeUI();
 
         user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null){
-            Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
+            Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         }
 
         signOutBtn.setOnClickListener(view -> {
             user = null;
             FirebaseAuth.getInstance().signOut();
-            Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
+            Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         });
     }
