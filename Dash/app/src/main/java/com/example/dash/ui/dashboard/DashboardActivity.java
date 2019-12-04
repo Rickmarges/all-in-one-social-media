7e3d722e51ecd7c5dc080adb826071e9d6f3ef58
MarynKaspers <marynkaspers20@gmail.com>
2019-12-04 21:33:35 +0100

    Update DashboardActivity.java
    Added functionality to spinner

--- Dash/app/src/main/java/com/example/dash/ui/dashboard/DashboardActivity.java
+++ Dash/app/src/main/java/com/example/dash/ui/dashboard/DashboardActivity.java
@@ -3,10 +3,13 @@ package com.example.dash.ui.dashboard;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuInflater;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.PopupMenu;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.dash.R;
@@ -14,8 +17,9 @@ import com.example.dash.ui.login.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     private Button signOutBtn;
+    private Spinner spinner;
     private FirebaseUser user;
     private int backCounter;
     private long startTime;
@@ -24,12 +28,12 @@ public class DashboardActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initialize();
+        setupSpinner();
     }
 
     @Override
     public void onResume(){
         super.onResume();
-        initialize();
     }
 
     @Override
@@ -57,15 +61,43 @@ public class DashboardActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
-        signOutBtn.setOnClickListener(view -> {
+//        signOutBtn.setOnClickListener(view -> {
+//            user = null;
+//            FirebaseAuth.getInstance().signOut();
+//            Intent intent = new Intent(this, LoginActivity.class);
+//            startActivity(intent);
+//        });
+    }
+
+    private void setupSpinner(){
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.menu, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(position == 2){
             user = null;
             FirebaseAuth.getInstance().signOut();
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
-        });
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
     private void initializeUI(){
         signOutBtn = findViewById(R.id.signOut);
+        spinner =  findViewById(R.id.spinner);
     }
+
 }
