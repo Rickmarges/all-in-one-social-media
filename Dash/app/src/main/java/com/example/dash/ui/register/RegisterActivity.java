dac93807c291fb897680477a28d31d3e8202aec7
Rickmarges <rickmarges@gmail.com>
2019-12-06 14:38:59 +0100

    Small fixes
    Fixed rounded logo
    Fixed rounded, animated add button
    Fixed back button on register page

--- Dash/app/src/main/java/com/example/dash/ui/register/RegisterActivity.java
+++ Dash/app/src/main/java/com/example/dash/ui/register/RegisterActivity.java
@@ -53,6 +53,13 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
     public void sendEmailVerification(){
         mAuth.getCurrentUser().sendEmailVerification()
             .addOnCompleteListener(task -> {
